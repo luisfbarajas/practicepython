8a8a8edf4d1d7ca03b6f3a7b3e61dab2b0b54bc6
lbarajas <luis.barajas@tress.com.mx>
2019-07-08 12:32:49 -0700

    screenshots of runnig programs

--- console/El-Ahorcado.py
+++ console/El-Ahorcado.py
@@ -59,7 +59,7 @@ IMAGENES_AHORCADO = ['''
    /|\  |
    / \  |
         |
- 58. =========''']
+  =========''']
 # string whit random words
 words = 'hormiga babuino tejon murcielago oso castor camello gato almeja cobra pantera coyote cuervo ciervo perro burro pato aguila huron zorro rana cabra ganso halcon leon lagarto llama topo mono alce raton mula salamandra nutria buho panda loro paloma piton conejo carnero rata cuervo rinoceronte salmon foca tiburon oveja mofeta perezoso serpiente araña cigüeña cisne tigre sapo trucha pavo tortuga comadreja ballena lobo wombat cebra'.split()
 
--- console/README.md
+++ console/README.md
@@ -1,3 +1,25 @@
 # Excersice python on console
 
 This programs are for practice on console side
+
+### Adivina el numero
+
+You have 6 trys tos guess the number generated between 0-20
+
+![Ahorcanumbero2](/console/img/number.PNG)
+
+### Cifrado cesar
+
+Encryp and unencryp a message that you choose 
+
+![cifrado](/console/img/cifrado-cesar.PNG)
+
+### El-Ahorcado
+
+The popular game, where you have to guess the word chosen of a list o words 
+
+![Ahorcado1](/console/img/ahorcado.PNG)
+
+![Ahorcado2](/console/img/ahorcado2.PNG)
+
+![Ahorcado3](/console/img/ahorcado3.PNG)
